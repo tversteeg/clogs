920a16496480f5f4de6c56f64cb99a2d3b039f2f
Thomas Versteeg <thomasversteeg@gmx.com>
2020-06-19 23:24:44 +0200

    Start designing main structure

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,1 +1,10 @@
-fn main() {}
+use anyhow::Result;
+use clogs::Clog;
+
+fn main() -> Result<()> {
+    let clog = Clog::new(800, 600)?;
+
+    clog.start();
+
+    Ok(())
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,46 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use anyhow::Result;
+use miniquad::{
+    conf::{Conf, Loading},
+    graphics::Context,
+    EventHandler, UserData,
+};
+
+/// The main game object.
+pub struct Clog {
+    /// The window width & height.
+    window_dimensions: (i32, i32),
+}
+
+impl Clog {
+    /// Setup a new game.
+    pub fn new(width: i32, height: i32) -> Result<Self> {
+        Ok(Self {
+            window_dimensions: (width, height),
+        })
+    }
+
+    /// Start the game.
+    pub fn start(self) {
+        miniquad::start(
+            Conf {
+                window_title: concat!("clog - ", env!("CARGO_PKG_VERSION")).to_string(),
+                window_width: self.window_dimensions.0,
+                window_height: self.window_dimensions.1,
+                loading: Loading::Embedded,
+                sample_count: 8,
+                ..Default::default()
+            },
+            |ctx| UserData::owning(self, ctx),
+        );
+    }
+}
+
+impl EventHandler for Clog {
+    fn update(&mut self, _: &mut Context) {
+        todo!()
+    }
+
+    fn draw(&mut self, _: &mut Context) {
+        todo!()
     }
 }
